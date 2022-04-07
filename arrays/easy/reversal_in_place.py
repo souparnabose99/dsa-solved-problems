fe9cc8093f8c14cd2cfbd8974cfb2283acfbb932
Souparna Bose <48193679+souparnabose99@users.noreply.github.com>
2022-04-07 09:47:33 +0530

    Update reversal_in_place.py

--- arrays/easy/reversal_in_place.py
+++ arrays/easy/reversal_in_place.py
@@ -1,1 +1,8 @@
+"""
+Problem Statement:
+@TODO:
+The problem is to reverse a T[] array in O(N) linear time complexity with the algorithm to be in-place.
+No additional memory can be used
 
+For example: input is [1,2,3,4,5,6,7,8] then the output is [8,7,6,5,4,3,2,1]
+"""
