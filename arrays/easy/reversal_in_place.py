ad5e1864d6f2b1ac7274dbf3ae22247337063e77
Souparna Bose <48193679+souparnabose99@users.noreply.github.com>
2022-04-07 09:47:55 +0530

    Update reversal_in_place.py

--- arrays/easy/reversal_in_place.py
+++ arrays/easy/reversal_in_place.py
@@ -6,3 +6,20 @@ No additional memory can be used
 
 For example: input is [1,2,3,4,5,6,7,8] then the output is [8,7,6,5,4,3,2,1]
 """
+
+
+# O(N) runtime complexity, O(1) space complexity
+def rev_array(arr):
+    i = 0
+    j = len(arr) - 1
+    while j >= i:
+        arr[i], arr[j] = arr[j], arr[i]
+        i += 1
+        j -= 1
+    return arr
+
+
+nums = [1, 2, 3, 4, 5, 6, 7, 8]
+print("Nums before reversal : ", nums)
+num_reversed = rev_array(nums)
+print("Nums after reversal : ", num_reversed)
