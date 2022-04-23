163d5d39cf1e01668979f52146d49e922fd056b0
Souparna Bose <48193679+souparnabose99@users.noreply.github.com>
2022-04-23 22:27:26 +0530

    Update queue_with_stack_recursion.py

--- queues/easy/queue_with_stack_recursion.py
+++ queues/easy/queue_with_stack_recursion.py
@@ -1,1 +1,41 @@
 
+
+"""
+@TODO:
+Construct an algorithm to implement queues with the help of a stack.
+O(N) additional memory usage allowed
+"""
+
+
+class Queue:
+
+    def __init__(self):
+        self.stack = []
+
+    def enqueue(self, data):
+        self.stack.append(data)
+
+    # 2 stacks are used, 2nd stack is the call stack of memory
+    def dequeue(self):
+        if len(self.stack) == 1:
+            return self.stack.pop()
+
+        item = self.stack.pop()
+
+        dequeued_item = self.dequeue()
+        self.stack.append(item)
+
+        return dequeued_item
+
+
+if __name__ == "__main__":
+    queue = Queue()
+    queue.enqueue(10)
+    queue.enqueue(30)
+    queue.enqueue(19)
+    queue.enqueue(79)
+    print(queue.dequeue())
+    print(queue.dequeue())
+    # 10
+    # 30
+
