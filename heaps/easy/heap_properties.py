5d15e82bd8819cc712c1216cfe26ce7bab35001f
Souparna Bose <48193679+souparnabose99@users.noreply.github.com>
2022-05-04 09:52:28 +0530

    Update heap_properties.py

--- heaps/easy/heap_properties.py
+++ heaps/easy/heap_properties.py
@@ -1,1 +1,16 @@
 
+
+def is_min_heap(heap):
+    # No need to check for leaf nodes: 2*i +2<= N, N = i-2/2
+    num_of_items = (len(heap) - 2) // 2
+    for i in range(num_of_items):
+        # Check heap property: Parent should be smaller than children for min heap
+        if heap[i] > heap[2*i + 1] or heap[i] > heap[2*i + 2]:
+            return False
+    return True
+
+
+hp = [1, 2, 3, 4, 5]
+print(is_min_heap(hp))
+
+
