14e4bb0dae64222faa11a60dea18f75d90e048fa
Souparna Bose <48193679+souparnabose99@users.noreply.github.com>
2022-05-07 13:46:49 +0530

    Update dfs_recursion.py

--- graphs/easy/dfs_recursion.py
+++ graphs/easy/dfs_recursion.py
@@ -1,1 +1,33 @@
 
+class Node:
+
+    def __init__(self, name):
+        self.name = name
+        self.adjacency_list = []
+        self.visited = False
+
+
+def dfs(node):
+    node.visited = True
+    print(node.name)
+
+    for n in node.adjacency_list:
+        if not n.visited:
+            dfs(n)
+
+
+if __name__ == "__main__":
+    node1 = Node("A")
+    node2 = Node("B")
+    node3 = Node("C")
+    node4 = Node("D")
+    node5 = Node("E")
+    node1.adjacency_list.append(node2)
+    node1.adjacency_list.append(node3)
+    node2.adjacency_list.append(node4)
+    node4.adjacency_list.append(node5)
+
+    # run DFS
+    dfs(node1)
+
+
