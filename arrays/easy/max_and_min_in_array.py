8a0ec6661929647446bb1e6feed2794ca394c009
Souparna Bose <48193679+souparnabose99@users.noreply.github.com>
2022-07-27 22:37:01 +0530

    Update max_and_min_in_array.py

--- arrays/easy/max_and_min_in_array.py
+++ arrays/easy/max_and_min_in_array.py
@@ -1,1 +1,31 @@
 
+"""
+@TODO :
+Given an array A of size N of integers. Your task is to find the minimum and maximum elements in the array.
+
+For example : N = 6, A[] = [3, 2, 1, 56, 10000, 167]
+Output: min = 1, max =  10000
+"""
+
+
+def max_min_in_arr(arr):
+    max_val = arr[0]
+    min_val = arr[0]
+
+    for i in range(len(arr)):
+        if arr[i] > max_val:
+            max_val = arr[i]
+
+        if arr[i] < min_val:
+            min_val = arr[i]
+
+    return max_val, min_val
+
+
+if __name__ == "__main__":
+    arr_1 = [3, 2, 1, 56, 10000, 167, 67, 28058, 34, -1]
+    max_v, min_v = max_min_in_arr(arr_1)
+    print("Max is : ", max_v)
+    print("Min is : ", min_v)
+
+
