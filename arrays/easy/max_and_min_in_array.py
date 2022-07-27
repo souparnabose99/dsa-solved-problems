15734f57c7a6b680c4f423f6bf1797fc36ce0e83
Souparna Bose <48193679+souparnabose99@users.noreply.github.com>
2022-07-27 22:40:16 +0530

    Update max_and_min_in_array.py

--- arrays/easy/max_and_min_in_array.py
+++ arrays/easy/max_and_min_in_array.py
@@ -21,8 +21,10 @@ def max_min_in_arr(arr):
 
     return max_val, min_val
 
+
 def max_min_in_arr_2(arr):
     return max(arr), min(arr)
+
 
 if __name__ == "__main__":
     arr_1 = [3, 2, 1, 56, 10000, 167, 67, 28058, 34, -1]
@@ -33,5 +35,4 @@ if __name__ == "__main__":
     max_v, min_v = max_min_in_arr_2(arr_1)
     print("Max is : ", max_v)
     print("Min is : ", min_v)
-
 
