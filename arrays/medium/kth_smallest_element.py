930c3ccb060941fe7f396d6290ee9a5f5da66910
Souparna Bose <48193679+souparnabose99@users.noreply.github.com>
2022-07-29 23:39:30 +0530

    Update kth_smallest_element.py

--- arrays/medium/kth_smallest_element.py
+++ arrays/medium/kth_smallest_element.py
@@ -1,6 +1,10 @@
 
 """
+@TODO:
 Given an array arr[] and an integer K where K is smaller than size of array, 
 the task is to find the Kth smallest element in the given array. 
 It is given that all array elements are distinct.
+
+For example : N = 6, arr[] = [7, 10, 4, 3, 20, 15], K = 3
+Output : 7
 """
