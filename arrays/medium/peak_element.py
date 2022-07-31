da013e9cca9f7c4d0647d99bf6c2a5054f300b05
Souparna Bose <48193679+souparnabose99@users.noreply.github.com>
2022-07-31 18:50:39 +0530

    Update peak_element.py

--- arrays/medium/peak_element.py
+++ arrays/medium/peak_element.py
@@ -3,26 +3,35 @@
 @TODO:
 An element is called a peak element if its value is not smaller than the value of its adjacent elements(if they exists).
 Given an array arr[] of size N, Return the index of any one of its peak elements.
-eg : N = 3
-arr[] = {1,2,3}
-Possible Answer: 2
-Explanation: index 2 is 3.
-It is the peak element as it is greater than its neighbour 2.
+
+For example : N = 4
+arr = [5, 10, 20, 15]
+Output : 20
 """
 
-def peakElement(self,arr, n):
-  # Code here
-  peak_element = 0
-  for i in range(n):
-    if i==0:
-      peak_element = 0
-    else:
-      if i<n-1 and (arr[i] > arr[i-1] and arr[i] > arr[i+1]):
-        peak_element = i
-  return peak_element
+
+def find_peak_element(arr):
+    for i in range(len(arr)):
+        if i == 0 and arr[i] > arr[i+1]:
+            return arr[i]
+        elif i == 1 and (arr[i] > arr[i-1] and arr[i] > arr[i+1]):
+            return arr[i]
+        elif i == (len(arr)-2) and (arr[i] > arr[i-1] and arr[i] > arr[i+1]):
+            return arr[i]
+        elif i == (len(arr)-1) and arr[i] > arr[i-1]:
+            return arr[i]
+        elif arr[i] > arr[i-1] and arr[i] > arr[i+1]:
+            return arr[i]
 
 
-# [3,4]
-# [1,2,3]
+if __name__ == "__main__":
+    # arr_1 = [5, 10, 20, 15]
+    # arr_1 = [10, 5, 20, 15]
+    arr_1 = [3, 17, 1, 0, 5, 6]
+    peak_element = find_peak_element(arr_1)
+    print("Peak Element is : ", peak_element)
 
+# Peak Element is :  20
+# Peak Element is :  10
+# Peak Element is :  17
 
