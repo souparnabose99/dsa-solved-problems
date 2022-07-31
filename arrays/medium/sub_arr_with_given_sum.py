92537f253e775843b688f5406b48e608995bd394
Souparna Bose <48193679+souparnabose99@users.noreply.github.com>
2022-07-31 19:11:45 +0530

    Update sub_arr_with_given_sum.py

--- arrays/medium/sub_arr_with_given_sum.py
+++ arrays/medium/sub_arr_with_given_sum.py
@@ -1,1 +1,10 @@
 
+"""
+@TODO:
+Given an unsorted array A of size N that contains only non-negative integers,
+find a continuous sub-array which adds to a given number S.
+In case of multiple sub-arrays, return the subarray which comes first on moving from left to right.
+
+For example : A = [1,2,3,4,5,6,7,8,9,10] , N = 10, S = 15
+Output: A[:5]
+"""
