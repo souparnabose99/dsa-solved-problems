510a4f2271a0aa6e457a24f4779f93fef480099e
Souparna Bose <48193679+souparnabose99@users.noreply.github.com>
2022-08-03 22:39:38 +0530

    Update cyclically_rotate_arr_by_one.py

--- arrays/medium/cyclically_rotate_arr_by_one.py
+++ arrays/medium/cyclically_rotate_arr_by_one.py
@@ -9,13 +9,23 @@ Output : [5, 1, 2, 3, 4]
 """
 
 
-def cyclic_arr_rotation():
-    pass
+def cyclic_arr_rotation(arr):
+    i = 0
+    j = len(arr) - 1
+    while j != 0:
+        arr[i], arr[j] = arr[j], arr[i]
+        i += 1
+        j -= 1
+    return arr
 
 
 if __name__ == "__main__":
-    cyclic_arr_rotation()
+    arr_1 = [9, 8, 7, 6, 4, 2, 1, 3]
+    print("Original Array : ", arr_1)
+    arr_final = cyclic_arr_rotation(arr_1)
+    print("Array after cyclic rotation : ", arr_final)
 
-
+    # Original Array :  [9, 8, 7, 6, 4, 2, 1, 3]
+    # Array after cyclic rotation :  [3, 8, 7, 6, 4, 2, 1, 9]
 
 
