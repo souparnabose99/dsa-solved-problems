310524226768b6e178f1cd6fb31dfdf95cf0930b
Souparna Bose <48193679+souparnabose99@users.noreply.github.com>
2022-08-04 22:33:36 +0530

    Update missing_num_in_array.py

--- arrays/medium/missing_num_in_array.py
+++ arrays/medium/missing_num_in_array.py
@@ -9,6 +9,7 @@ Output: 9
 """
 
 
+# O(N) runtime complexity
 def find_missing_num(arr):
     for i in range(1, len(arr) +1):
         if i in arr:
@@ -20,8 +21,11 @@ def find_missing_num(arr):
 
 if __name__ == "__main__":
     arr_1 = [6, 1, 2, 8, 3, 4, 7, 10, 5]
+    arr_2 = [1, 2, 3, 5]
     num = find_missing_num(arr_1)
-    
+    num_2 = find_missing_num(arr_2)
+
     # The missing number is :  9
+    # The missing number is :  4
 
 
