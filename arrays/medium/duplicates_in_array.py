ed977d535bedef56520f344120a1f3152be23a45
Souparna Bose <48193679+souparnabose99@users.noreply.github.com>
2022-08-07 19:02:41 +0530

    Update duplicates_in_array.py

--- arrays/medium/duplicates_in_array.py
+++ arrays/medium/duplicates_in_array.py
@@ -1,1 +1,36 @@
 
+
+"""
+@TODO:
+Given an array a[] of size N which contains elements from 0 to N-1,
+you need to find all the elements occurring more than once in the given array.
+
+For example:
+N = 5, A = [2, 3, 1, 2, 3], Output: 2, 3
+"""
+
+
+def find_duplicates(arr):
+    dup_arr = list()
+    for i in range(len(arr)):
+        if arr.count(i) > 1:
+            dup_arr.append(i)
+    if len(dup_arr) > 1:
+        return dup_arr
+    else:
+        return -1
+
+
+if __name__ == "__main__":
+    arr_1 = [1, 2, 3]
+    print("Original array : ", arr_1)
+    arr_2 = find_duplicates(arr_1)
+    print("Array with duplicates : ", arr_2)
+
+
+# Original array :  [2, 3, 1, 2, 3]
+# Array with duplicates :  [2, 3]
+# Original array :  [1, 2, 3]
+# Array with duplicates :  -1
+
+
