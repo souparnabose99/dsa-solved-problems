c16c2306e83a62c9933f51776780d4c7a95294f6
Souparna Bose <48193679+souparnabose99@users.noreply.github.com>
2022-08-18 23:08:11 +0530

    Update reverse_linked_list.py

--- linked-list/medium/reverse_linked_list.py
+++ linked-list/medium/reverse_linked_list.py
@@ -1,1 +1,12 @@
 
+"""
+@TODO:
+Given a linked list of N nodes. The task is to reverse this list.
+For example:
+LinkedList: 1->2->3->4->5->6
+After reversing the list, elements are 6->5->4->3->2->1.
+"""
+
+
+
+
