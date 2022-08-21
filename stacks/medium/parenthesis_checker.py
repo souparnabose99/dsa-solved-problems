e202d9dc01ffcdce7f17e76b5b07a8f5e3fe9389
Souparna Bose <48193679+souparnabose99@users.noreply.github.com>
2022-08-21 23:40:43 +0530

    Update parenthesis_checker.py

--- stacks/medium/parenthesis_checker.py
+++ stacks/medium/parenthesis_checker.py
@@ -1,1 +1,27 @@
 
+class Stack:
+
+    def __init__(self):
+        self.stack = []
+
+    def push(self, data):
+        self.stack.append(data)
+
+    def pop(self):
+        if self.size_of_stack() < 1:
+            return
+        data = self.stack[-1]
+        del self.stack[-1]
+        return data
+
+    def peek(self):
+        return self.stack[-1]
+
+    def is_empty(self):
+        return self.stack == []
+
+    def size_of_stack(self):
+        return len(self.stack)
+
+
+stack = Stack()
